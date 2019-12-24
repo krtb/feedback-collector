0a97c0df9ced7d23234c81c7eded4a4221751d86
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-23 21:11:14 -0500

    add route handler for /auth/google

--- index.js
+++ index.js
@@ -20,9 +20,17 @@ passport.use(new GoogleStrategy(
     }
 ));
 
-console.log('App is running on port')
+// user comes to this route,
+// managed by passport,
+// asking for specific scopes from users google account
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
 
 // HEROKU injects env vars, after deployment
 // OR, if there isn't a prod env variable, set it to be 5000 for local dev
 const PORT = process.env.PORT || 5000
+console.log(`App is running on port ${PORT}`)
 app.listen(PORT)
