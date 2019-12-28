964d85d9aa7f5c5dda1bb00a752640291a1c95e8
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-28 15:51:56 -0500

    console.log user profile information

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ passport.use(new GoogleStrategy(
     callbackURL: 'http://localhost:5000/auth/google/callback'
     }, 
     (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken, 'HERE IS ACCESS TOKEN');
+
+        console.log('ACCESS TOKEN', accessToken);
+        console.log('REFRESH TOKEN', refreshToken);
+        console.log('profile:', profile);
+        
         done(null, profile);
     }
 ));
