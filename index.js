4da8fd3e06aca1b63b6eedb33aec78efabaa39dd
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-30 17:26:08 -0500

    view user profile attributes in console

--- index.js
+++ index.js
@@ -9,14 +9,38 @@ const app = express()
 
 // passport.use = make passport aware of new strategy
 // create new instance of Google Passport Strategy, pass in configurations
-passport.use(new GoogleStrategy({
+passport.use(new GoogleStrategy(
+    {
     clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret
-}));
+    clientSecret: keys.googleClientSecret,
+    callbackURL: 'http://localhost:5000/auth/google/callback'
+    }, 
+    (accessToken, refreshToken, profile, done) => {
 
-console.log('App is running on port')
+        console.log('ACCESS TOKEN', accessToken);
+        console.log(refreshToken, 'REFRESH TOKEN');
+        console.log(profile, 'profile');
+        
+        done(null, profile);
+    }
+));
+
+// user comes to this route,
+// managed by passport,
+// asking for specific scopes from users google account
+app.get('/auth/google', 
+    passport.authenticate('google', {
+    scope: [
+        'profile', 'email'
+    ]
+    }
+    )
+)
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 // HEROKU injects env vars, after deployment
 // OR, if there isn't a prod env variable, set it to be 5000 for local dev
 const PORT = process.env.PORT || 5000
+console.log(`App is running on port ${PORT}`)
 app.listen(PORT)
