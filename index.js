ceaf6ff1ab336d1b53023d860b587cedffd4cb8e
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-30 22:30:41 -0500

    condense to require because nothing being returned

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // import express = makes use of ES2015 modules
 const express = require('express')
 const app = express()
+require('./services/passport')
 
 // HEROKU injects env vars, after deployment
 // OR, if there isn't a prod env variable, set it to be 5000 for local dev
