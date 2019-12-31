1e6f32f82375dfaf5b4ccca5c0010422964cb92b
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-30 22:19:39 -0500

    move auth routes, passport to seperate folders

--- index.js
+++ index.js
@@ -7,38 +7,6 @@ const keys = require('./config/keys') // require keys object
 
 const app = express()
 
-// passport.use = make passport aware of new strategy
-// create new instance of Google Passport Strategy, pass in configurations
-passport.use(new GoogleStrategy(
-    {
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: 'http://localhost:5000/auth/google/callback'
-    }, 
-    (accessToken, refreshToken, profile, done) => {
-
-        console.log('ACCESS TOKEN', accessToken);
-        console.log('REFRESH TOKEN', refreshToken);
-        console.log('profile:', profile);
-        
-        done(null, profile);
-    }
-));
-
-// user comes to this route,
-// managed by passport,
-// asking for specific scopes from users google account
-app.get('/auth/google', 
-    passport.authenticate('google', {
-    scope: [
-        'profile', 'email'
-    ]
-    }
-    )
-)
-
-app.get('/auth/google/callback', passport.authenticate('google'))
-
 // HEROKU injects env vars, after deployment
 // OR, if there isn't a prod env variable, set it to be 5000 for local dev
 const PORT = process.env.PORT || 5000
