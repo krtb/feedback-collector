b7e13eb357d0cd1d2522e76b45dc995931140985
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-31 11:52:17 -0500

    call authRoutes with the app object in index

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // require = using common JS modules,
 // import express = makes use of ES2015 modules
 const express = require('express')
-const app = express()
 require('./services/passport')
+// below takes app object and attaches two routes to it
+const authRoutes = require('./routes/authRoutes')
+const app = express()
+
+// call authRoutes with the app object
+authRoutes(app)
 
 // HEROKU injects env vars, after deployment
 // OR, if there isn't a prod env variable, set it to be 5000 for local dev
