717bd0a0fcd13a4cec2718b2450903095f0b0ede
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-30 22:22:40 -0500

    remove unsused require statements

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 // require = using common JS modules,
 // import express = makes use of ES2015 modules
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const keys = require('./config/keys') // require keys object
-
 const app = express()
 
 // HEROKU injects env vars, after deployment
