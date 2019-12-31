888c525d6785d530ff63dce2d73de0a624d8870b
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-30 22:23:19 -0500

    add passport,keys require statements

--- services/passport.js
+++ services/passport.js
@@ -1,3 +1,7 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys') // require keys object
+
 // passport.use = make passport aware of new strategy
 // create new instance of Google Passport Strategy, pass in configurations
 passport.use(new GoogleStrategy(
