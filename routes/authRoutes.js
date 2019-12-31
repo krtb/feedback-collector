cb6f61ac54ad0cbef80caa61f1a58d74019693ad
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-31 11:46:58 -0500

    export functions with module.exports, pass in app

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,13 +4,15 @@ const passport = require('passport')
 // user comes to this route,
 // managed by passport,
 // asking for specific scopes from users google account
-app.get('/auth/google',
-    passport.authenticate('google', {
-        scope: [
-            'profile', 'email'
-        ]
-    }
+module.exports = (app) => {
+    app.get('/auth/google',
+        passport.authenticate('google', {
+            scope: [
+                'profile', 'email'
+            ]
+        }
+        )
     )
-)
 
-app.get('/auth/google/callback', passport.authenticate('google'))
+    app.get('/auth/google/callback', passport.authenticate('google'))
+}
