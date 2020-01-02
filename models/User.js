da29d79f8b9592474061e96f8868adef981fd6bd
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-02 10:22:58 -0500

    add inline comment on adding properties

--- models/User.js
+++ models/User.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose'); 
 const { Schema } = mongoose;
 
+// can freely as properties
 const userSchema  = new Schema({
    googleId: String, 
-})
+});
 
 mongoose.model('users', userSchema);
