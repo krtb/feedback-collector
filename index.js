2eb9a36ac76287ca99166da3d36a7eafb217e1b0
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-01 22:10:46 -0500

    refactor authRoutes require, remove variable

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
 const express = require('express')
 require('./services/passport')
 // below takes app object and attaches two routes to it
-const authRoutes = require('./routes/authRoutes')
 const app = express()
 
 // call authRoutes with the app object
-authRoutes(app)
+// require returns a function, what was written in the file
+// then immediately call that function with the app object
+require('./routes/authRoutes')(app)
 
 // HEROKU injects env vars, after deployment
 // OR, if there isn't a prod env variable, set it to be 5000 for local dev
