88d405b0a8473f3bb4375c911d4e6dadcde60a27
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-02 10:38:38 -0500

    add User mongoose model to passport.js

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,12 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose');
 const keys = require('../config/keys') // require keys object
+
+// use model calss to create new instance of a User
+// testing envs, like mocha, cause issue w/Mongoose when repeatedly calling it
+// instead of require, will use different method
+const User = mongoose.model('users');
 
 // passport.use = make passport aware of new strategy
 // create new instance of Google Passport Strategy, pass in configurations
