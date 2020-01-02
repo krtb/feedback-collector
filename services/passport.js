735ebfd37688eba54e790fc2a87a38829b07fa11
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-02 10:41:17 -0500

    create new User instance, save() to DB

--- services/passport.js
+++ services/passport.js
@@ -17,11 +17,7 @@ passport.use(new GoogleStrategy(
         callbackURL: 'http://localhost:5000/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-
-        console.log('ACCESS TOKEN', accessToken);
-        console.log('REFRESH TOKEN', refreshToken);
-        console.log('profile', profile);
-
-        done(null, profile);
+        // profile.id is coming from signed in user Google profile
+        new User ({ googleId: profile.id }).save();
     }
 ));
