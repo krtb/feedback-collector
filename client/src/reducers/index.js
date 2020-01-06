4e1fbd3078f678e12347876f1029d47628be7db2
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-05 22:47:09 -0500

    add auth reducer, keu of authReducer from import

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 
+// whatever keys are passed to this object,
+// will represent keys inside of state object
 export default combineReducers({
-
+    auth: authReducer
 });
