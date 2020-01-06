2e846b93b2ca29e15f38be6d92f4f3c6a8064743
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-06 09:10:07 -0500

    add comment on responsibility of authReducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,5 @@
+// RESPONSIBLE FOR: recording whether or not a user is logged in
+
 // takes two args: STATE & ACTION
 // state is for this reducer, action is action object with different types
 export default function(state={}, action) {
