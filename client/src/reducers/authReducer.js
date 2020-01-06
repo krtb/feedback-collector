8aac1b71ab79968726d38c22d2da8f2b66073e2a
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-05 22:39:51 -0500

    add state and action arguments to function

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,5 @@
-export default function() {
-    
+// takes two args: STATE & ACTION
+// state is for this reducer, action is action object with different types
+export default function(state, action) {
+
 }
