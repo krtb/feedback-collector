7b80fd4814bd303c2fa81f3fb1ee658f85c278ec
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-05 22:41:30 -0500

    provide state w/default value, return in switch

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,9 @@
 // takes two args: STATE & ACTION
 // state is for this reducer, action is action object with different types
-export default function(state, action) {
+export default function(state={}, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
 
 }
