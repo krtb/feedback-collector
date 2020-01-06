c43e43f01dc3957da9a7ba7c4dbddbb72da41fc4
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-05 22:22:06 -0500

    import createStore,applyMiddleware for Provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'; // start setup of redux
+import { createStore, applyMiddleware } from 'redux'; // then pass store to provider to connect react & redux 
 
 import App from './components/App'
 
