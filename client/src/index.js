ddd497ae6f30a104f18b52b7be509ba24beb78ec
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-05 22:49:24 -0500

    import reducers, pass into createStore function

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,9 @@ import { Provider } from 'react-redux'; // start setup of redux. Provider makes 
 import { createStore, applyMiddleware } from 'redux'; // then pass store to provider to connect react & redux 
 
 import App from './components/App'
+import reducers from './reducers'
 
-const store = createStore(() => [], {}, applyMiddleware()) // next create a new instance of redux STORE. first arg of createStore is all reducers
+const store = createStore(reducers, {}, applyMiddleware()) // next create a new instance of redux STORE. first arg of createStore is all reducers
 
 // fourth step, to wrap app in provider, add store
 ReactDOM.render(
