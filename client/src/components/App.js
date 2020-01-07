7417384d61fdcad0ae522c8e4074368b9ca395f1
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-07 12:08:15 -0500

    wrap app with container styling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,7 +11,7 @@ const Landing = () => <h2>Landing</h2>
 
 const App = () => {
     return(
-        <div>
+        <div className="container" >
             <BrowserRouter>
                 {/* always show route by treating as regular component */}
                 <Header />
