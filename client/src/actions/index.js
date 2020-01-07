8a5dab7a52f2985c8315ab031e904f9264e60728
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-07 13:09:45 -0500

    add inline comment

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,6 +3,6 @@ import {FETCH_USER} from './types';
 
 const fetch_user = () => {
     // in dev environment, make user of proxy, proxy sends response back to react app
-    // in prod, this address would go directly to our express backend
+    // in prod, no create-react-app server, this address would go directly to our express backend
     axios.get('/api/current_user')
 }
