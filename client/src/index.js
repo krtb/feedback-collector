c52d37aebb29a0680a76b447ca636dc9bb2297f3
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-07 11:12:15 -0500

    css not being assignged to var, remove to condense

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 // doesn't matter where you import CSS min file
-import materizalizeCSS from 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'; // start setup of redux. Provider makes store accessible to every component in the app
