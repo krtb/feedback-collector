f6977906a344cd043c9469066390c0b357aa564d
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-07 12:11:56 -0500

    import redux-thunk into index.js, use in store

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'; // start setup of redux. Provider makes store accessible to every component in the app
 import { createStore, applyMiddleware } from 'redux'; // then pass store to provider to connect react & redux 
+import reduxThunk from 'redux-thunk'; //middleware
 
 import App from './components/App'
 import reducers from './reducers'
 
-const store = createStore(reducers, {}, applyMiddleware()) // next create a new instance of redux STORE. first arg of createStore is all reducers
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk)) // next create a new instance of redux STORE. first arg of createStore is all reducers
 
 // fourth step, to wrap app in provider, add store
 ReactDOM.render(
