7c86bb1fca021d11d2b3d083550829fa07866668
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-07 13:37:51 -0500

    refactor functional to class based component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 // import uses Webpack & Babel, giving us access to ES2015 modules
-import React from 'react';
+import React, { Component } from 'react';
 // BrowserRouter --> looks at url, changes set of components on screen
 // Route --> route objects sets up rule between route and set cof components
 import {BrowserRouter, Route} from 'react-router-dom'; // react router helpers to help navigate around dom
@@ -9,24 +9,26 @@ const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => {
-    return(
-        <div className="container" >
-            <BrowserRouter>
-                {/* always show route by treating as regular component */}
-                <Header />
+class App extends Component {
+    render(){
+        return (
+            <div className="container" >
+                <BrowserRouter>
+                    {/* always show route by treating as regular component */}
+                    <Header />
 
-                {/* use exact={true} to force Router to only use one route */}
-                <Route exact={true} path="/" component={Landing} />
+                    {/* use exact={true} to force Router to only use one route */}
+                    <Route exact={true} path="/" component={Landing} />
 
-                <Route exact path="/surveys" component={Dashboard} />
+                    <Route exact path="/surveys" component={Dashboard} />
 
-                <Route path="/surveys/new" component={SurveyNew} />
+                    <Route path="/surveys/new" component={SurveyNew} />
 
 
-            </BrowserRouter>
-        </div>
-    );
+                </BrowserRouter>
+            </div>
+        )
+    }
 }
 
 export default App;
