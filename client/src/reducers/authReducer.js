e370271a146bcc8a3c6eff67c58ba3735f17d3cd
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-08 10:48:26 -0500

    modify to return null,false, or action.payload

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,10 +1,17 @@
 // RESPONSIBLE FOR: recording whether or not a user is logged in
 
-// takes two args: STATE & ACTION
-// state is for this reducer, action is action object with different types
-export default function(state={}, action) {
+import { FETCH_USER } from '../actions/types';
+
+// 3 scenerios for auth reducer
+// 1 - returns null - pending promise resolution
+// 2 - returns User model - request complete, user logged in
+// 3 - return false - request done, use not logged in
+
+export default function(state=null, action) {
     console.log(action, 'actions FROM authReducer')
     switch (action.type) {
+        case FETCH_USER:
+            return action.payload || false;
         default:
             return state;
     }
