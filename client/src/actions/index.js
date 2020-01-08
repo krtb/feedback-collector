4e272235f23c7e07e13597036bb84eb7a62e832e
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-07 21:00:25 -0500

    refactor function to use dispatch, async/await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,22 +1,7 @@
 import axios from 'axios';
 import {FETCH_USER} from './types';
 
-// 1. when action creator called, returns a function
-// 2. redux-thunk sees that we return a function, automatically calls it with a dispatch
-// 3. make a request with axios
-// 4. wait till we get a response
-// 5. then dispatch our action
 
-const fetch_user = () => {
-    // in dev environment, make user of proxy, proxy sends response back to react app
-    // in prod, no create-react-app server, this address would go directly to our express backend
-
-    // only want to dispatch actions once the ASYNC REQUEST completes
-    return function(dispatch){
-        // when this func is executed, will return actual request
-        // when action creator gets called, will instantly return a function
-        // purpose of redux thunk is to inspect whatever action we return from our action creator
-        axios.get('/api/current_user')
-            .then(res => dispatch ({ type: FETCH_USER, payload: res  }))
-    }
+export const fetchUser = () => async (dispatch) => { 
+    dispatch({ type: FETCH_USER, payload: await axios.get('/api/current_user')  }) 
 }
