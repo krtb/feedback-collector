c2f5845a841c6d35780745a3ea65896cfb28af8b
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-09 14:41:40 -0500

    create Landing functional component

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return(
+        <div style={{textAlign: 'center'}} >
+            <h1>
+                Feedback Collector
+            </h1>
+            Collect feedback from your clients
+        </div>
+    )
+}
+
+export default Landing;
