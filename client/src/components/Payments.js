f1e0a21aadd67c1faf29bf3612fce4cafa3668e5
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-10 10:32:03 -0500

    create payments class component

---
+++ client/src/components/Payments.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payments extends Components {
+    render() {
+        return(
+            <div>
+                PAYMENT COMPONENT
+            </div>
+        )
+    }
+}
+
+export default Payments
