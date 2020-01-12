6b9b81ca92dc1b168ce26f06aa9366e42dc5326c
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-12 10:40:59 -0500

    create request to stripe for charge to user

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,7 +3,12 @@ const stripe = require('stripe');
 
 module.exports = app => {
     // create Stripe post request handler for user credit payment
-    app.post('api/stripe', (req, res) => {
-        
+    app.post('/api/stripe', (req, res) => {
+        stripe.charges.create({
+            amount: 500,
+            currency: 'usd',
+            description: '$5 for 5 credits',
+            source: req.body.id
+        })
     })
 }
