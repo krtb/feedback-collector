6274a4bffd8c8822b7bca65be8cf5651a81952ec
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-12 11:49:22 -0500

    create Stripe charge object for payments

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,14 +1,30 @@
 const keys = require('../config/keys');
-const stripe = require('stripe');
+const requireLogin = require('../middlewares/requireLogin');
+const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
     // create Stripe post request handler for user credit payment
-    app.post('/api/stripe', (req, res) => {
-        stripe.charges.create({
+    app.post('/api/stripe', requireLogin, async (req, res) => {
+
+
+
+        const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
             description: '$5 for 5 credits',
             source: req.body.id
         })
+        // after successful charge to User credit card
+        // take User model, add 5 credits to them, send User model back to the client
+        // respond to request with newly updated User model
+
+        // setup with passport.initialize() and passport.session()
+        // User model
+        req.user.credits += 5; 
+        //reference copy of User that was just saved to DB
+        const user = await req.user.save(); //async request
+        // send response back to User
+        res.send(user);
+        
     })
 }
