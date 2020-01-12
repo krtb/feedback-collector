b1e523c127faba2bf6946e6ba43f60e62d516c55
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-11 19:35:39 -0500

    change single to double paranthesis

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,5 +1,5 @@
 const keys = require("../config/keys");
-const stripe = require('stripe');
+const stripe = require("stripe");
 
 module.exports = app => {
     // create Stripe post request handler for user credit payment
