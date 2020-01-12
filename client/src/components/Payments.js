f5b5192ff33969b06a1a05dcdafc0275ae26ca3f
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-11 19:05:11 -0500

    wire up handleToken action creator

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -11,7 +11,7 @@ class Payments extends Component {
                     name="Feedback Collector"
                     description="$5 for 5 email credits"
                     amount={500}
-                    token={token => console.log(token)}
+                    token={token => this.props.handleToken(token)}
                     stripeKey={process.env.REACT_APP_STRIPE_KEY}
                 >
                     <button className="btn" >
